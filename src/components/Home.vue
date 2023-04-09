7fc9ad591727d7ef05c04277b540a826a54b56f2
luis <luis@na.edu>
2023-04-08 20:02:00 -0500

    vue initial commit to remote repo

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBar from './NavBar.vue';
+import {ref} from 'vue';
+let showPopup = ref<boolean>(false);
+function handleClickPopup(){
+    console.log(showPopup);
+    showPopup.value = true;
+}
+</script>
+<template>
+<div class="h-full">
+    <NavBar/>
+    <slot></slot>
+</div>
+</template>
+<style scoped>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+const navLink = ref([
+    'Luis',
+    'Home'
+])
+</script>
+<template >
+    <nav class="p-6 bg-slate-600 w-48 h-full">
+        <ul class="">
+            <li v-for="item in navLink">{{item}}</li>
+        </ul>
+    </nav>
+</template>
